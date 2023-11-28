30e9112bc9058b0b8f2df48e68275e1e845bed36
David Sarten <bsarten@mddevice.com>
2023-11-28 11:35:13 -0500

    converted to a CMake project for build performance

--- 1/Sources/main.swift
+++ 1/Sources/main.swift
@@ -1,4 +1,17 @@
 // The Swift Programming Language
 // https://docs.swift.org/swift-book
 
-print("Hello, world!")
+import Foundation
+
+// let input_path = Process().currentDirectoryURL!.path + "/../../1/input.txt"
+// if freopen(input_path, "r", stdin) == nil {
+//     perror(input_path)
+//     exit(1)
+// }
+// while let line = readLine() {
+//     print(line)
+// }
+
+// var g = blah()
+// print(g.test)
+print("Hello, World!")
